5629a87d7252073382b336733d3595ee1c67793c
xps13\paco <pacodelacruz@gmail.com>
2017-05-01 22:36:30 +1000

    Added comments to classes

--- PacodelaCruz.PollingConsumer.FunctionApp/Shared/PollingWatermark.cs
+++ PacodelaCruz.PollingConsumer.FunctionApp/Shared/PollingWatermark.cs
@@ -2,6 +2,9 @@
 
 namespace PacodelaCruz.PollingConsumer.FunctionApp
 {
+    /// <summary>
+    /// This class will help us to wrap the PollingWatermarkEntity and make it more user friendly. By using the constructor, we are naming the PartitionKey as SourceSystem, and the RowKey as Entity. Additionally, we are returning another property called NextWatermark that is to be used as the upper bound when querying the source system and when updating the Polling Watermark after we have successfully polled the source system
+    /// </summary>
     public class PollingWatermark
     {
         public string SourceSystem { get; set; }
@@ -9,9 +12,7 @@ namespace PacodelaCruz.PollingConsumer.FunctionApp
         public DateTime Watermark { get; set; }
         public DateTime NextWatermark { get; set; }
         public PollingWatermark()
-        {
-
-        }
+        { }
 
         public PollingWatermark(PollingWatermarkEntity pollingWatermarkEntity)
         {
--- PacodelaCruz.PollingConsumer.FunctionApp/Shared/PollingWatermarkEntity.cs
+++ PacodelaCruz.PollingConsumer.FunctionApp/Shared/PollingWatermarkEntity.cs
@@ -3,10 +3,20 @@ using Microsoft.WindowsAzure.Storage.Table;
 
 namespace PacodelaCruz.PollingConsumer.FunctionApp
 {
+    /// <summary>
+    /// Class to handle the entities on the Azure Table Storage called ‘PollingWatermark'. 
+    /// If you want to learn more about using Azure Storage Tables with C# have a look at this documentation
+    /// https://docs.microsoft.com/en-us/azure/storage/storage-dotnet-how-to-use-tables
+    /// </summary>
     public class PollingWatermarkEntity : TableEntity
     {
         public PollingWatermarkEntity() { }
 
+        /// <summary>
+        /// We are storing the SourceSystem in the PartitionKey and the Entity in the RowKey
+        /// </summary>
+        /// <param name="sourceSystem"></param>
+        /// <param name="entity"></param>
         public PollingWatermarkEntity(string sourceSystem, string entity)
         {
             this.PartitionKey = sourceSystem;
